8ed028d1a3841fc67ecf724a05c1ece16ec799f3
alcaesar2000 <sabah1oftheiraq@gmail.com>
2025-07-19 02:02:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 // إعداد Firebase
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    appId: "YOUR_APP_ID"
+npm install firebase
 };
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
